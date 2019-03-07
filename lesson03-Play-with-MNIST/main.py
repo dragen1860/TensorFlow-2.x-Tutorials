a1319d39f9bf69d887b1b8728e326ca02cb7954e
dragen1860 <dragen@live.cn>
2019-03-07 17:58:36 +1100

    update

---
+++ lesson03-Play-with-MNIST/main.py
@@ -0,0 +1,55 @@
+import  tensorflow as tf
+from    tensorflow.keras import datasets, layers, optimizers, Sequential, metrics
+
+
+
+
+(xs, ys),_ = datasets.mnist.load_data()
+print('datasets:', xs.shape, ys.shape, xs.min(), xs.max())
+
+
+xs = tf.convert_to_tensor(xs, dtype=tf.float32) / 255.
+db = tf.data.Dataset.from_tensor_slices((xs,ys))
+db = db.batch(32).repeat(10)
+
+
+network = Sequential([layers.Dense(256, activation='relu'),
+                     layers.Dense(256, activation='relu'),
+                     layers.Dense(256, activation='relu'),
+                     layers.Dense(10)])
+network.build(input_shape=(None, 28*28))
+network.summary()
+
+optimizer = optimizers.SGD(lr=0.01)
+acc_meter = metrics.Accuracy()
+
+for step, (x,y) in enumerate(db):
+
+    with tf.GradientTape() as tape:
+        # [b, 28, 28] => [b, 784]
+        x = tf.reshape(x, (-1, 28*28))
+        # [b, 784] => [b, 10]
+        out = network(x)
+        # [b] => [b, 10]
+        y_onehot = tf.one_hot(y, depth=10)
+        # [b, 10]
+        loss = tf.square(out-y_onehot)
+        # [b]
+        loss = tf.reduce_sum(loss) / 32
+
+
+    acc_meter.update_state(tf.argmax(out, axis=1), y)
+
+    grads = tape.gradient(loss, network.trainable_variables)
+    optimizer.apply_gradients(zip(grads, network.trainable_variables))
+
+
+    if step%200==0:
+
+        print(step, 'loss:', float(loss), 'acc:', acc_meter.result().numpy())
+        acc_meter.reset_states()
+
+
+
+
+
