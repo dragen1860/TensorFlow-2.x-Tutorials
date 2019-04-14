044d6492851b9fb1e8568a97cd0b45250e1e0703
ash3n <33986672+ash3n@users.noreply.github.com>
2019-04-13 22:50:05 -0500

    GPT model

--- lesson28-Transformer/main.py
+++ lesson28-Transformer/main.py
@@ -1,5 +1,6 @@
 import os
 import math
+import collections
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
@@ -38,8 +39,8 @@ args.afn = gelu
 zeros_init = keras.initializers.Zeros()
 ones_init = keras.initializers.Ones()
 
-
 class LayerNorm(keras.Model):
+    """Construct a layernorm module in the OpenAI style (epsilon inside the square root)."""
 
     def __init__(self, n_state=768, e=1e-5):
         super(LayerNorm, self).__init__()
@@ -83,7 +84,7 @@ class Attention(keras.Model):
         n_state = nx # in Attention: n_state = 768 (nx=n_emb)
          # [switch nx => n_state from Block to Attention to keep identical to openai implem]
         assert n_state % cfg.n_head == 0
-        self.b = self.add_weight(shape=[1, 1, n_ctx, n_ctx], initializer=ones_init)
+        self.b = self.add_weight(shape=[1, 1, n_ctx, n_ctx], initializer=ones_init) # register buffer
         self.b.assign(tf.linalg.LinearOperatorLowerTriangular(self.b).to_dense())
         self.n_head = cfg.n_head
         #self.split_size = n_state
@@ -164,11 +165,13 @@ class Block(keras.Model):
 
 
 class TransformerModel(keras.Model):
+    """ Transformer model """
 
-    def __init__(self, cfg=args, vocab=40990, n_ctx=512):
+    def __init__(self, cfg=args, vocab=40558, n_ctx=512):
         super(TransformerModel, self).__init__()
         self.vocab = vocab
         self.embed = keras.layers.Embedding(vocab, cfg.n_embd)
+        self.embed.build([1])
         self.drop = keras.layers.Dropout(cfg.embd_pdrop)
         self.h = [Block(n_ctx, cfg, scale=True) for _ in range(cfg.n_layer)]
 
@@ -180,3 +183,156 @@ class TransformerModel(keras.Model):
         for block in self.h:
             h = block(h)
         return h
+
+
+class LMHead(keras.Model):
+    """ Language Model Head for the transformer """
+
+    def __init__(self, model, cfg=args, trunc_and_reshape=True):
+        super(LMHead, self).__init__()
+        self.n_embd = cfg.n_embd
+        embed_shape = model.embed.weights[0].shape
+        self.embed = model.embed.weights[0]
+        self.decoder = lambda x: tf.matmul(x, tf.transpose(self.embed))
+        self.trunc_and_reshape = trunc_and_reshape  # XD
+
+    def call(self, h):
+        # Truncated Language modeling logits (we remove the last token)
+        h_trunc = tf.reshape(h[:, :-1], [-1, self.n_embd]) \
+            if self.trunc_and_reshape else h  # XD
+        lm_logits = self.decoder(h_trunc)
+        return lm_logits
+
+
+class MultipleChoiceHead(keras.Model):
+    """ Classifier Head for the transformer """
+
+    def __init__(self, clf_token=40480, cfg=args):
+        super(MultipleChoiceHead, self).__init__()
+        self.n_embd = cfg.n_embd
+        self.n_ctx = cfg.n_ctx
+        self.clf_token = clf_token
+        self.dropout = keras.layers.Dropout(cfg.clf_pdrop, [1, 2, cfg.n_embd, 1]) # might need to change the 1s to smth else
+        self.linear = keras.layers.Dense(1, input_shape=[cfg.n_embd], 
+            kernel_initializer=keras.initializers.RandomNormal(stddev=0.02), 
+            bias_initializer=keras.initializers.RandomNormal(stddev=1))
+        self.linear.build([cfg.n_embd])
+
+    def call(self, h, x):
+        # Classification logits
+        clf_h = tf.reshape(h, [-1, self.n_embd])
+        flat = tf.reshape(x[..., 0], [-1])
+        clf_h = tf.boolean_mask(clf_h, tf.equal(flat, self.clf_token))
+        clf_h = tf.reshape(clf_h, [-1, x.shape[1], self.n_embd, 1])
+        # This double transposition is there to replicate the behavior
+        # of the noise_shape argument in the tensorflow
+        # implementation.  For more details, see
+        # https://github.com/huggingface/pytorch-openai-transformer-lm/issues/11
+        # clf_h = self.dropout(clf_h.transpose(1, 2)).transpose(1, 2)
+        clf_h = self.dropout(clf_h)
+        clf_h = tf.reshape(clf_h, [-1, self.n_embd])
+        clf_logits = self.linear(clf_h)
+
+        return tf.reshape(clf_logits, [-1, x.shape[1]])
+
+
+class ClfHead(keras.Model):
+    """Classification Head for the transformer
+
+    TODO: test this class."""
+    def __init__(self, clf_token=40480, cfg=args, n_class=10):
+        super(ClfHead, self).__init__()
+        self.n_embd = cfg.n_embd
+        self.clf_token = clf_token
+        self.dropout = keras.layers.Dropout(cfg.clf_pdrop)
+        self.linear = keras.layers.Dense(n_class, input_shape=[cfg.n_embd], 
+            kernel_initializer=keras.initializers.RandomNormal(stddev=0.02), 
+            bias_initializer=keras.initializers.RandomNormal(stddev=1))
+
+    def call(self, h, x):
+        clf_h = tf.reshape(h, [-1, self.n_embd])
+        flat = tf.reshape(x[..., 0], [-1])
+        clf_h = clf_h[flat == self.clf_token, :]
+        clf_h = tf.boolean_mask(clf_h, tf.equal(flat, self.clf_token))
+        clf_h = self.dropout(clf_h)
+        clf_logits = self.linear(clf_h)
+
+        return clf_logits
+
+
+class SimilarityHead(keras.Model):
+    """ Similarity Head for the transformer
+
+        TODO: test this class."""
+    def __init__(self, clf_token=40480, cfg=args):
+        super(SimilarityHead, self).__init__()
+        self.n_embd = cfg.n_embd
+        self.clf_token = clf_token
+        self.dropout = keras.layers.Dropout(cfg.clf_pdrop)
+        self.linear = keras.layers.Dense(n_class, input_shape=[cfg.n_embd], 
+            kernel_initializer=keras.initializers.RandomNormal(stddev=0.02), 
+            bias_initializer=keras.initializers.RandomNormal(stddev=1))
+
+    def call(self, h, x):
+        sim_h = tf.reshape(h, [-1, self.n_embd])
+        flat = tf.reshape(x[..., 0], [-1])
+        sim_h = tf.boolean_mask(sim_h, tf.equal(flat, self.clf_token))
+        sim_h = self.dropout(sim_h)
+        sim_h = tf.reduce_sum(sim_h, 1)
+        sim_logits = self.linear(sim_h)
+
+        return sim_logits
+
+
+class LMModel(keras.Model):
+    """ Transformer with language model head only """
+    def __init__(self, cfg=args, vocab=40990, n_ctx=512, return_probs=False):
+        super(LMModel, self).__init__()
+        self.transformer = TransformerModel(cfg, vocab=vocab, n_ctx=n_ctx)
+        self.lm_head = LMHead(self.transformer, cfg, trunc_and_reshape=False)
+        self.return_probs = return_probs
+        if self.return_probs:
+            pos_emb_mask = tf.zeros([1, 1, vocab]) # register buffer
+            pos_emb_mask[:, :, -n_ctx:] = -1e12
+
+    def call(self, x):
+        h = self.transformer(x)
+        lm_logits = self.lm_head(h)
+        if self.return_probs:
+            lm_logits = tf.nn.softmax(lm_logits + self.pos_emb_mask, -1)
+        return lm_logits
+
+
+class DoubleHeadModel(keras.Model):
+    """ Transformer with language model and task specific heads """
+    def __init__(self, cfg=args, clf_token=40480, task_head_type='multiple_choice', vocab=40990, n_ctx=512):
+        super(DoubleHeadModel, self).__init__()
+        self.transformer = TransformerModel(cfg, vocab=vocab, n_ctx=n_ctx)
+        self.lm_head = LMHead(self.transformer, cfg)
+        if isinstance(task_head_type, str):
+            if task_head_type == 'multiple_choice':
+                self.task_head = MultipleChoiceHead(clf_token, cfg)
+            elif task_head_type == 'similarity':
+                self.task_head = SimilarityHead(clf_token, cfg)
+            elif task_head_type == 'inference':
+                # the three classes correspond to entailment, contradiction and neutral.
+                self.task_head = ClfHead(clf_token, cfg, 3)
+            else:
+                raise ValueError("task_head_type is expected to be 'multiple_choice' "
+                                 "'similarity', 'inference' or ('classification', n_class) "
+                                 f"got {task_head_type}.")
+        elif isinstance(task_head_type, collections.abc.Sequence) and len(task_head_type) == 2 and \
+             task_head_type[0] == 'classification':
+            n_class = task_head_type[1]
+            self.task_head = ClfHead(clf_token, cfg, n_class)
+        else:
+            raise ValueError("task_head_type is expected to be 'multiple_choice' "
+                             "'similarity', 'inference' or ('classification', n_class) "
+                             f"got {task_head_type}.")
+
+    def call(self, x):
+        h = self.transformer(x)
+        lm_logits = self.lm_head(h)
+        task_logits = self.task_head(h, x)
+
+        return lm_logits, task_logits
