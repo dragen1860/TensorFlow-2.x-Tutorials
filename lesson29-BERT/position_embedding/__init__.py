70663860611e7dbfca03a0af088c6c4dea2d23c4
ash3n <33986672+ash3n@users.noreply.github.com>
2019-04-21 00:11:02 -0500

    BERT self attention

---
+++ lesson29-BERT/position_embedding/__init__.py
@@ -0,0 +1,2 @@
+from .pos_embd import PositionEmbedding
+from .trig_pos_embd import TrigPosEmbedding
---
+++ lesson29-BERT/position_embedding/pos_embd.py
@@ -0,0 +1,134 @@
+from tensorflow import keras
+import tensorflow.keras.backend as K
+
+
+class PositionEmbedding(keras.layers.Layer):
+    """Turn integers (positions) into dense vectors of fixed size.
+    eg. [[-4], [10]] -> [[0.25, 0.1], [0.6, -0.2]]
+
+    Expand mode: negative integers (relative position) could be used in this mode.
+        # Input shape
+            2D tensor with shape: `(batch_size, sequence_length)`.
+
+        # Output shape
+            3D tensor with shape: `(batch_size, sequence_length, output_dim)`.
+
+    Add mode:
+        # Input shape
+            3D tensor with shape: `(batch_size, sequence_length, feature_dim)`.
+
+        # Output shape
+            3D tensor with shape: `(batch_size, sequence_length, feature_dim)`.
+
+    Concat mode:
+        # Input shape
+            3D tensor with shape: `(batch_size, sequence_length, feature_dim)`.
+
+        # Output shape
+            3D tensor with shape: `(batch_size, sequence_length, feature_dim + output_dim)`.
+    """
+    MODE_EXPAND = 'expand'
+    MODE_ADD = 'add'
+    MODE_CONCAT = 'concat'
+
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 mode=MODE_EXPAND,
+                 embeddings_initializer='uniform',
+                 embeddings_regularizer=None,
+                 activity_regularizer=None,
+                 embeddings_constraint=None,
+                 mask_zero=False,
+                 **kwargs):
+        """
+        :param input_dim: The maximum absolute value of positions.
+        :param output_dim: The embedding dimension.
+        :param embeddings_initializer:
+        :param embeddings_regularizer:
+        :param activity_regularizer:
+        :param embeddings_constraint:
+        :param mask_zero: The index that represents padding. Only works in `append` mode.
+        :param kwargs:
+        """
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.mode = mode
+        self.embeddings_initializer = keras.initializers.get(embeddings_initializer)
+        self.embeddings_regularizer = keras.regularizers.get(embeddings_regularizer)
+        self.activity_regularizer = keras.regularizers.get(activity_regularizer)
+        self.embeddings_constraint = keras.constraints.get(embeddings_constraint)
+        self.mask_zero = mask_zero
+        self.supports_masking = mask_zero is not False
+
+        self.embeddings = None
+        super(PositionEmbedding, self).__init__(**kwargs)
+
+    def get_config(self):
+        config = {'input_dim': self.input_dim,
+                  'output_dim': self.output_dim,
+                  'mode': self.mode,
+                  'embeddings_initializer': keras.initializers.serialize(self.embeddings_initializer),
+                  'embeddings_regularizer': keras.regularizers.serialize(self.embeddings_regularizer),
+                  'activity_regularizer': keras.regularizers.serialize(self.activity_regularizer),
+                  'embeddings_constraint': keras.constraints.serialize(self.embeddings_constraint),
+                  'mask_zero': self.mask_zero}
+        base_config = super(PositionEmbedding, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def build(self, input_shape):
+        if self.mode == self.MODE_EXPAND:
+            self.embeddings = self.add_weight(
+                shape=(self.input_dim * 2 + 1, self.output_dim),
+                initializer=self.embeddings_initializer,
+                name='embeddings',
+                regularizer=self.embeddings_regularizer,
+                constraint=self.embeddings_constraint,
+            )
+        else:
+            self.embeddings = self.add_weight(
+                shape=(self.input_dim, self.output_dim),
+                initializer=self.embeddings_initializer,
+                name='embeddings',
+                regularizer=self.embeddings_regularizer,
+                constraint=self.embeddings_constraint,
+            )
+        super(PositionEmbedding, self).build(input_shape)
+
+    def compute_mask(self, inputs, mask=None):
+        if self.mode == self.MODE_EXPAND:
+            if self.mask_zero:
+                output_mask = K.not_equal(inputs, self.mask_zero)
+            else:
+                output_mask = None
+        else:
+            output_mask = mask
+        return output_mask
+
+    def compute_output_shape(self, input_shape):
+        if self.mode == self.MODE_EXPAND:
+            return input_shape + (self.output_dim,)
+        if self.mode == self.MODE_CONCAT:
+            return input_shape[:-1] + (input_shape[-1] + self.output_dim,)
+        return input_shape
+
+    def call(self, inputs, **kwargs):
+        if self.mode == self.MODE_EXPAND:
+            if K.dtype(inputs) != 'int32':
+                inputs = K.cast(inputs, 'int32')
+            return K.gather(
+                self.embeddings,
+                K.minimum(K.maximum(inputs, -self.input_dim), self.input_dim) + self.input_dim,
+            )
+        input_shape = K.shape(inputs)
+        if self.mode == self.MODE_ADD:
+            batch_size, seq_len, output_dim = input_shape[0], input_shape[1], input_shape[2]
+        else:
+            batch_size, seq_len, output_dim = input_shape[0], input_shape[1], self.output_dim
+        pos_embeddings = K.tile(
+            K.expand_dims(self.embeddings[:seq_len, :self.output_dim], axis=0),
+            [batch_size, 1, 1],
+        )
+        if self.mode == self.MODE_ADD:
+            return inputs + pos_embeddings
+        return K.concatenate([inputs, pos_embeddings], axis=-1)
