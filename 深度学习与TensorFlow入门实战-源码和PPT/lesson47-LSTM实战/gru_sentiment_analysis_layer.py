4757602e5424236b4fc85ef9c0b140622c9a3e5f
Jackie Loong <dragen@live.cn>
2019-07-14 23:08:15 +1000

    add bn and lstm

---
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson47-LSTM实战/gru_sentiment_analysis_layer.py
@@ -0,0 +1,115 @@
+import  os
+import  tensorflow as tf
+import  numpy as np
+from    tensorflow import keras
+from    tensorflow.keras import layers
+
+
+tf.random.set_seed(22)
+np.random.seed(22)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+assert tf.__version__.startswith('2.')
+
+batchsz = 128
+
+# the most frequest words
+total_words = 10000
+max_review_len = 80
+embedding_len = 100
+(x_train, y_train), (x_test, y_test) = keras.datasets.imdb.load_data(num_words=total_words)
+# x_train:[b, 80]
+# x_test: [b, 80]
+x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=max_review_len)
+x_test = keras.preprocessing.sequence.pad_sequences(x_test, maxlen=max_review_len)
+
+db_train = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+db_train = db_train.shuffle(1000).batch(batchsz, drop_remainder=True)
+db_test = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+db_test = db_test.batch(batchsz, drop_remainder=True)
+print('x_train shape:', x_train.shape, tf.reduce_max(y_train), tf.reduce_min(y_train))
+print('x_test shape:', x_test.shape)
+
+
+
+class MyRNN(keras.Model):
+
+    def __init__(self, units):
+        super(MyRNN, self).__init__()
+
+
+        # transform text to embedding representation
+        # [b, 80] => [b, 80, 100]
+        self.embedding = layers.Embedding(total_words, embedding_len,
+                                          input_length=max_review_len)
+
+        # [b, 80, 100] , h_dim: 64
+        self.rnn = keras.Sequential([
+            # layers.SimpleRNN(units, dropout=0.5, return_sequences=True, unroll=True),
+            # layers.SimpleRNN(units, dropout=0.5, unroll=True)
+
+            # unroll: Boolean (default False). If True, the network will be unrolled,
+            # else a symbolic loop will be used.
+            # Unrolling can speed-up a RNN, although it tends to be more memory-intensive.
+            # Unrolling is only suitable for short sequences.
+            layers.GRU(units, dropout=0.5, return_sequences=True, unroll=True),
+            layers.GRU(units, dropout=0.5, unroll=True)
+        ])
+
+
+        # fc, [b, 80, 100] => [b, 64] => [b, 1]
+        self.outlayer = layers.Dense(1)
+
+    def call(self, inputs, training=None):
+        """
+        net(x) net(x, training=True) :train mode
+        net(x, training=False): test
+        :param inputs: [b, 80]
+        :param training:
+        :return:
+        """
+        # [b, 80]
+        x = inputs
+        # embedding: [b, 80] => [b, 80, 100]
+        x = self.embedding(x)
+        # rnn cell compute
+        # x: [b, 80, 100] => [b, 64]
+        x = self.rnn(x)
+
+        # out: [b, 64] => [b, 1]
+        x = self.outlayer(x)
+        # p(y is pos|x)
+        prob = tf.sigmoid(x)
+
+        return prob
+
+def main():
+    units = 64
+    epochs = 4
+
+    import time
+
+
+    t0 = time.time()
+    model = MyRNN(units)
+    model.compile(optimizer = keras.optimizers.Adam(0.001),
+                  loss = tf.losses.BinaryCrossentropy(),
+                  metrics=['accuracy'])
+
+    model.fit(db_train, epochs=epochs, validation_data=db_test)
+
+    model.evaluate(db_test)
+
+
+    t1 = time.time()
+    # Unroll=True
+    # LSTM: 69.3 secnods, 83%
+    # GRU: 100 seconds, 83.4%
+
+    # Unroll=False
+    # LSTM：23.71， 81.24
+    # GRU 23.05， 83.11
+    print('total time cost:', t1-t0)
+
+
+if __name__ == '__main__':
+    main()
