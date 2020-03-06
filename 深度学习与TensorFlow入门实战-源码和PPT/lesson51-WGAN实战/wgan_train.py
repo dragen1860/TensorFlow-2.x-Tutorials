9ca1187f918c4c32b7f2371077120e4f73b153d8
Jackie Loong <dragen@live.cn>
2020-03-06 22:56:45 +0800

    Update wgan_train.py

--- 深度学习与TensorFlow入门实战-源码和PPT/lesson51-WGAN实战/wgan_train.py
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson51-WGAN实战/wgan_train.py
@@ -1,9 +1,9 @@
 import  os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import  numpy as np
 import  tensorflow as tf
 from    tensorflow import keras
+
 from    PIL import Image
 import  glob
 from    gan import Generator, Discriminator
@@ -38,25 +38,24 @@ def save_result(val_out, val_block_size, image_path, color_mode):
             single_row = np.array([])
 
     if final_image.shape[2] == 1:
-        final_image = np.squeeze(final_image, axis=2)
-    # toimage(final_image).save(image_path)
+        final_image = np.squeeze(final_image, axis=2) 
     Image.fromarray(final_image).save(image_path)
 
 
 def celoss_ones(logits):
     # [b, 1]
     # [b] = [1, 1, 1, 1,]
-    loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits,
-                                                   labels=tf.ones_like(logits))
-    return tf.reduce_mean(loss)
+    # loss = tf.keras.losses.categorical_crossentropy(y_pred=logits,
+    #                                                y_true=tf.ones_like(logits))
+    return - tf.reduce_mean(logits)
 
 
 def celoss_zeros(logits):
     # [b, 1]
     # [b] = [1, 1, 1, 1,]
-    loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits,
-                                                   labels=tf.zeros_like(logits))
-    return tf.reduce_mean(loss)
+    # loss = tf.keras.losses.categorical_crossentropy(y_pred=logits,
+    #                                                y_true=tf.zeros_like(logits))
+    return tf.reduce_mean(logits)
 
 
 def gradient_penalty(discriminator, batch_x, fake_image):
@@ -95,7 +94,7 @@ def d_loss_fn(generator, discriminator, batch_z, batch_x, is_training):
     d_loss_fake = celoss_zeros(d_fake_logits)
     gp = gradient_penalty(discriminator, batch_x, fake_image)
 
-    loss = d_loss_fake + d_loss_real + 1. * gp
+    loss = d_loss_real + d_loss_fake + 10. * gp
 
     return loss, gp
 
@@ -108,11 +107,11 @@ def g_loss_fn(generator, discriminator, batch_z, is_training):
 
     return loss
 
+
 def main():
 
-    tf.random.set_seed(22)
-    np.random.seed(22)
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    tf.random.set_seed(233)
+    np.random.seed(233)
     assert tf.__version__.startswith('2.')
 
 
@@ -120,11 +119,13 @@ def main():
     z_dim = 100
     epochs = 3000000
     batch_size = 512
-    learning_rate = 0.002
+    learning_rate = 0.0005
     is_training = True
 
 
-    img_path = glob.glob(r'C:\Users\Jackie Loong\Downloads\DCGAN-LSGAN-WGAN-GP-DRAGAN-Tensorflow-2-master\data\faces\*.jpg')
+    img_path = glob.glob(r'C:\Users\Jackie\Downloads\faces\*.jpg')
+    assert len(img_path) > 0
+    
 
     dataset, img_shape, _ = make_anime_dataset(img_path, batch_size)
     print(dataset, img_shape)
@@ -135,26 +136,30 @@ def main():
     db_iter = iter(dataset)
 
 
-    generator = Generator()
+    generator = Generator() 
     generator.build(input_shape = (None, z_dim))
     discriminator = Discriminator()
     discriminator.build(input_shape=(None, 64, 64, 3))
+    z_sample = tf.random.normal([100, z_dim])
 
-    g_optimizer = tf.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5)
-    d_optimizer = tf.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5)
+
+    g_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5)
+    d_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5)
 
 
     for epoch in range(epochs):
 
-        batch_z = tf.random.uniform([batch_size, z_dim], minval=-1., maxval=1.)
-        batch_x = next(db_iter)
+        for _ in range(5):
+            batch_z = tf.random.normal([batch_size, z_dim])
+            batch_x = next(db_iter)
 
-        # train D
-        with tf.GradientTape() as tape:
-            d_loss, gp = d_loss_fn(generator, discriminator, batch_z, batch_x, is_training)
-        grads = tape.gradient(d_loss, discriminator.trainable_variables)
-        d_optimizer.apply_gradients(zip(grads, discriminator.trainable_variables))
-
+            # train D
+            with tf.GradientTape() as tape:
+                d_loss, gp = d_loss_fn(generator, discriminator, batch_z, batch_x, is_training)
+            grads = tape.gradient(d_loss, discriminator.trainable_variables)
+            d_optimizer.apply_gradients(zip(grads, discriminator.trainable_variables))
+        
+        batch_z = tf.random.normal([batch_size, z_dim])
 
         with tf.GradientTape() as tape:
             g_loss = g_loss_fn(generator, discriminator, batch_z, is_training)
@@ -165,7 +170,7 @@ def main():
             print(epoch, 'd-loss:',float(d_loss), 'g-loss:', float(g_loss),
                   'gp:', float(gp))
 
-            z = tf.random.uniform([100, z_dim])
+            z = tf.random.normal([100, z_dim])
             fake_image = generator(z, training=False)
             img_path = os.path.join('images', 'wgan-%d.png'%epoch)
             save_result(fake_image.numpy(), 10, img_path, color_mode='P')
